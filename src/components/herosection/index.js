b796f7c8e580f25e6802e53afa1f4f6cf61eda77
Joshua Martinez <mrjoshuamartinez@gmail.com>
2020-12-02 02:12:50 -0500

    styled hero section and button style

--- src/components/herosection/index.js
+++ src/components/herosection/index.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Video from '../../videos/video.mp4';
-import { HeroContainer, HeroBg, VideoBg } from './herosection'; 
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from './herosection';
+import { Button } from '../buttonstyle';
 
 const HeroSection = () => {
+  const [ hover, setHover ] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
   return (
     <HeroContainer>
       <HeroBg>
         <VideoBg autoPlay loop muted src={ Video } type='video/mp4' />
       </HeroBg>
+      <HeroContent>
+        <HeroH1>Full Stack Developer</HeroH1>
+        <HeroP>
+          A glance into a developers mindset.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to='projects'
+            onMouseEnter={ onHover }
+            onMouseLeave={ onHover }
+          >
+            View Projects { hover ? <ArrowForward /> : <ArrowRight /> }
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   )
 }
